f232f4a19354f55b29ee2a1c85b3473f3b3196ea
Cedrick320 <54862979+Cedrick320@users.noreply.github.com>
2019-09-10 11:19:30 -0400

    Add files via upload

--- dom.js
+++ dom.js
@@ -1,2 +1,16 @@
-var item = document.getElementsByClassName('row');
-console.log(item);
+
+//Variables box  types d'immeubles
+
+var resid = document.getElementById("residential");
+var com= document.getElementById("commercial");
+var corp= document.getElementById("corp");
+var hybrid= document.getElementById("hybrid");
+resid.style.display='block';
+
+var on_change = function() {
+    var select = document.getElementById("selector").value;
+ if (select=="1"){
+     resid.style.display='block';
+ }
+
+};
