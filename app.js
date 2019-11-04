baa354ff7fde5eb8cf0777d4ece173af5f8a6353
Cedrick320 <cedgag1@gmail.com>
2019-11-04 12:49:43 -0500

    ne

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express()
-
+app.get('/',(req,res) => {
+res.send(console.log =("Hello World!!!"))
+})
 app.get('/tender',(req,res) => {
 res.send([1,2,3])
 
 })
 //PORT
- const port = portprocess.env.PORT || 3000;
+ const port = process.env.PORT || 3000;
+//const port = 3000
 app.listen(port,() => console.log(`Listening to port ${port}....`))
